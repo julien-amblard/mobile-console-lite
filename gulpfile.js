502e813bddb9dcd97289986a7832c20ceeda11c5
Julien Amblard <julien.amblard@seloger.com>
2019-09-09 15:50:41 +0200

    test 2

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const livereload 		= require('gulp-livereload');
 const babel 			= require('gulp-babel');
 const watch 			= require('gulp-watch');
 const rename 			= require('gulp-rename');
-//test
+
 gulp.task('watch', () => {
 	gulp.watch('src/*.js', ['js']);
 	livereload.listen();
